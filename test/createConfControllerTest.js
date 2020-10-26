1b41db39e37a6b52a40621c3b8d9350e211a796a
Estelle Comment <estelle.comment@gmail.com>
2020-10-26 15:10:43 +0100

    Add sinon stubs

--- test/createConfControllerTest.js
+++ test/createConfControllerTest.js
@@ -1,8 +1,25 @@
 const chai = require('chai')
+const sinon = require('sinon')
 
-const app = require('../index');
+const app = require('../index')
+const conferences = require('../lib/conferences')
+const emailer = require('../lib/emailer')
 
 describe('createConfController', function() {
+  beforeEach(function(done) {
+    this.createConfStub = sinon.stub(conferences, 'createConf')
+        .returns(Promise.resolve({ phoneNumber: '0122334455', id: 123456}))
+    this.sendEmailStub = sinon.stub(emailer, 'sendConfCreatedEmail')
+        .returns(Promise.resolve())
+    done()
+  })
+
+  afterEach(function(done) {
+    this.createConfStub.restore()
+    this.sendEmailStub.restore()
+    done()
+  })
+
   it('should refuse invalid email', function(done) {
     chai.request(app)
       .post('/create-conf')
@@ -12,6 +29,8 @@ describe('createConfController', function() {
       })
       .end((err, res) => {
         res.should.redirectTo('http://127.0.0.1:8080/')
+        sinon.assert.notCalled(this.createConfStub)
+        sinon.assert.notCalled(this.sendEmailStub)
         done()
       })
   })
