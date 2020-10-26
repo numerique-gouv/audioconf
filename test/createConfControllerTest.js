e06f7d8f61203773bfe90cc91a8c90533acace0c
Estelle Comment <estelle.comment@gmail.com>
2020-10-26 15:00:50 +0100

    Basic test with chai-http

--- test/createConfControllerTest.js
+++ test/createConfControllerTest.js
@@ -1,9 +1,18 @@
-const assert = require('assert')
+const chai = require('chai')
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
+const app = require('../index');
+
+describe('createConfController', function() {
+  it('should refuse invalid email', function(done) {
+    chai.request(app)
+      .post('/create-conf')
+      .type('form')
+      .send({
+        email: 'bad.email',
+      })
+      .end((err, res) => {
+        res.should.redirectTo('http://127.0.0.1:8080/')
+        done()
+      })
   })
 })
