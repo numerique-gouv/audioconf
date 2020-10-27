e5c5927ee7922b75194bd42d4cf37c121ed59174
Estelle Comment <estelle.comment@gmail.com>
2020-10-27 11:19:10 +0100

    Validate email from accepted list. Regex list is gitignored.

--- controllers/createConfController.js
+++ controllers/createConfController.js
@@ -1,14 +1,44 @@
-const emailer = require('../lib/emailer')
 const conferences = require('../lib/conferences')
+const config = require('../config')
+const emailer = require('../lib/emailer')
 
-module.exports.createConf = async (req, res) => {
-  const email = req.body.email
 
+let isAcceptedEmail = undefined
+try {
+  const emailRegexes = require('../emailRegexes').regexes
+  isAcceptedEmail = (email) => {
+    for (const regex of emailRegexes) {
+      if (regex.test(email)) {
+        return true
+      }
+    }
+    return false
+  }
+} catch (ex) {
+  // No emailRegexes file. No validation to run, everything is accepted.
+  isAcceptedEmail = (email) => true
+}
+
+const isValidEmail = (email) => {
   if (
     email === undefined ||
     !/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email)
   ) {
+    return false
+  }
+  return true
+}
+
+module.exports.createConf = async (req, res) => {
+  const email = req.body.email
+
+  if (isValidEmail(email)) {
     req.flash('error', 'Email invalide. Avez vous bien tapé votre email ? Vous pouvez réessayer.')
+    return res.redirect('/')
+  }
+
+  if (!isAcceptedEmail(email)) {
+    req.flash('error', `Cet email ne correspond pas à une agence de l\'Etat. Vous ne pouvez pas utiliser ${config.APP_NAME} avec cet email.`)
     return res.redirect('/')
   }
 
