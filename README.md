cf466ebdb98cdb1070a664cf263704aa627a9bf5
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2020-11-01 21:47:52 +0100

    Add extension in knew

--- README.md
+++ README.md
@@ -36,9 +36,6 @@ Pour utiliser d'autres commandes, le [CLI de KnexJS](http://knexjs.org/#Migratio
 
 ## Docker compose
 
-- Initialiser la base de donnée : 
-    - `docker-compose up -d db`
-    - `docker-compose exec db psql -U conferences -d conferences -c 'CREATE EXTENSION "uuid-ossp";'`
 - Récupérer les dépendences : `docker-compose run -u root web npm install`
 - Créer les tables de la DB : `docker-compose run web npm run migrate`
 - Créer une migration : `docker-compose run web npm run makeMigration <nom de la migration>`
--- migrations/20201101122909_clean-conferences-table.js
+++ migrations/20201101122909_clean-conferences-table.js
@@ -2,6 +2,9 @@
 exports.up = function(knex) {
     return knex.schema
         .dropTable('conferences')
+        .then(() =>{
+            return knex.raw('CREATE EXTENSION "uuid-ossp";')
+        })
         .then(() => {
             return knex.schema.createTable('conferences', (table) => {
                 table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
@@ -18,6 +21,9 @@ exports.up = function(knex) {
 exports.down = function(knex) {
     return knex.schema
     .dropTable('conferences')
+    .then(() => {
+        return knex.raw('DROP EXTENSION IF EXISTS "uuid-ossp";')
+    })
     .then(() => {
         return knex.schema.createTable('conferences', (table) => {
             table.increments().primary();
