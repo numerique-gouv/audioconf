a00b0e4114ac2d69134bfcf369021bff87c6cbe0
Estelle Comment <estelle.comment@gmail.com>
2020-11-04 12:12:38 +0100

    Differentiate case where conf is expired

--- controllers/createConfController.js
+++ controllers/createConfController.js
@@ -85,7 +85,13 @@ module.exports.showConf = async (req, res) => {
 module.exports.cancelConf = async (req, res) => {
   const confId = req.params.id
   try {
-    const conference = await db.cancelConference(confId)
+    const conference = module.exports.getConference(id)
+    if (!conference || conference.expiresAt > new Date()) {
+      req.flash('error', 'La conférence a expiré. Vous pouvez recréer une conférence.')
+      return res.redirect('/')
+    }
+
+    await db.cancelConference(confId)
 
     req.flash('success', 'La conférence a bien été annulée. Si vous avez encore besoin d\'une conférence, vous pouvez en créer une nouvelle.')
     console.log(`La conférence ${confId} a été annulée`)
