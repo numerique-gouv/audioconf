6c622deb0bed6c3cbd35621b0e14fe57d29570ca
Estelle Comment <estelle.comment@gmail.com>
2020-11-16 13:50:38 +0100

    Make the other durationInMinutes field, in conferences table, nullable

--- migrations/20201116111022_make-duration-nullable.js
+++ migrations/20201116111022_make-duration-nullable.js
@@ -2,12 +2,24 @@ exports.up = knex => {
   return knex.schema
     .alterTable('loginTokens', (table) => {
       table.string('durationInMinutes').nullable().alter()
+    }).then(() => {
+      return knex.schema
+        .alterTable('conferences', (table) => {
+          table.string('durationInMinutes').nullable().alter()
+        })
     })
 }
 
+// Note : if the column contains null values, the rollback with fail :
+// you cannot set notNullable when there are null values.
 exports.down = knex => {
   return knex.schema
-    .alterTable('imloginTokensages', table => {
+    .alterTable('loginTokens', table => {
       table.string('durationInMinutes').notNullable().alter()
+    }).then(() => {
+      return knex.schema
+        .alterTable('conferences', table => {
+          table.string('durationInMinutes').notNullable().alter()
+        })
     })
 }
