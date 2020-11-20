fef298507f07aca11d1b98b7bbe619671e22df31
Estelle Comment <estelle.comment@gmail.com>
2020-11-20 17:47:17 +0100

    Test OVH status with Numbers API

--- controllers/statusController.js
+++ controllers/statusController.js
@@ -1,3 +1,6 @@
+const conferences = require('../lib/conferences')
+const config = require("../config")
+
 /*
 GET /status
 code 200 -> tout va bien
@@ -5,6 +8,14 @@ code 500 -> erreur
 */
 module.exports.getStatus = async (req, res) => {
   // todo : tester la connexion à la base de donnée
-  // todo : tester la connexion à OVH
+  try {
+    if (!config.USE_OVH_ROOM_API) {
+      await conferences.getAllPhoneNumbers()
+    }
+  } catch(err) {
+    console.log('status check got err', err)
+    return res.status(500).json({ message: 'error with OVH', error: err.toString() })
+  }
+
   res.status(200).json({ message: 'OK' })
 }
