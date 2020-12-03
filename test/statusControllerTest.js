c6a0b28b5c67878f8c070158cd058600ab8ce608
Estelle Comment <estelle.comment@gmail.com>
2020-12-03 11:20:43 +0100

    Add test for failing OVH

--- test/statusControllerTest.js
+++ test/statusControllerTest.js
@@ -2,14 +2,21 @@ const chai = require('chai')
 const sinon = require('sinon')
 
 const app = require('../index')
+const conferences = require('../lib/conferences')
+const config = require('../config')
 const urls = require('../urls')
 
 describe('statusController', function() {
+  config.USE_OVH_ROOM_API = true
+  let getRoomsStatsStub
   beforeEach(function(done) {
+    getRoomsStatsStub = sinon.stub(conferences, 'getRoomsStats')
+      .returns(Promise.resolve({coucou: "123"}))
     done()
   })
 
   afterEach(function(done) {
+    getRoomsStatsStub.restore()
     done()
   })
 
@@ -20,6 +27,22 @@ describe('statusController', function() {
         chai.assert(res.status === 200, 'HTTP status is 200')
         chai.assert(res.body.status, 'status is true')
         chai.assert(res.body.OVHStatus, 'OVHStatus is true')
+        chai.assert(res.body.DBStatus, 'DBStatus is true')
+        done()
+      })
+  })
+
+  it('should return 500 when OVH is down', function(done) {
+    getRoomsStatsStub.restore()
+    getRoomsStatsStub = sinon.stub(conferences, 'getRoomsStats')
+      .returns(Promise.reject('OVH is dowwwwwwnnnnn'))
+
+    chai.request(app)
+      .get(urls.status)
+      .end((err, res) => {
+        chai.assert(res.status === 500, 'HTTP status is 500')
+        chai.assert(!res.body.status, 'status is false')
+        chai.assert(!res.body.OVHStatus, 'OVHStatus is false')
         chai.assert(res.body.DBStatus, 'DBStatus is true')
         done()
       })
