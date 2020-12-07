9a4e0db5b90cc142e04ee56b9b117b9153fb2f2f
pom421 <mauguet.po@gmail.com>
2020-12-07 14:22:39 +0100

    doc: comments

--- jobs/anonymizeConferences/script.js
+++ jobs/anonymizeConferences/script.js
@@ -1,3 +1,4 @@
+// script to run manually the job. Used only in package.json scripts.
 const anonymizeConferences = require("./index")
 
 anonymizeConferences()
--- jobs/computeStats/script.js
+++ jobs/computeStats/script.js
@@ -1,3 +1,4 @@
+// script to run manually the job. Used only in package.json scripts.
 const computeStats = require("./index")
 
 computeStats()
