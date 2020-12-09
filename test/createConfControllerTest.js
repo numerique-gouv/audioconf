b893db75f7d066ed8498900b82e3ecbe88fb3697
Estelle Comment <estelle.comment@gmail.com>
2020-12-09 17:06:35 +0100

    Fix test for success case for createConfController

--- test/createConfControllerTest.js
+++ test/createConfControllerTest.js
@@ -3,7 +3,7 @@ const sinon = require('sinon')
 
 const app = require('../index')
 const conferences = require('../lib/conferences')
-const config = require('../config')
+const db = require('../lib/db')
 const emailer = require('../lib/emailer')
 const testUtils = require('./utils')
 const urls = require('../urls')
@@ -11,17 +11,28 @@ const urls = require('../urls')
 describe('createConfController', function() {
   let createConfStub
   let sendEmailStub
+  let getTokenStub
+  let insertConfStub
   beforeEach(function(done) {
     createConfStub = sinon.stub(conferences, 'createConf')
-        .returns(Promise.resolve({ phoneNumber: '0122334455', id: 123456}))
+        .returns(Promise.resolve({ phoneNumber: '+330122334455', pin: 123456789, freeAt: new Date() }))
     sendEmailStub = sinon.stub(emailer, 'sendConfCreatedEmail')
         .returns(Promise.resolve())
+    getTokenStub = sinon.stub(db, 'getToken')
+        .returns(Promise.resolve([{
+          email: 'good.email@thing.com',
+          conferenceDay: '2020-12-09',
+        }]))
+    insertConfStub = sinon.stub(db, 'insertConferenceWithFreeAt')
+
     done()
   })
 
   afterEach(function(done) {
     createConfStub.restore()
     sendEmailStub.restore()
+    getTokenStub.restore()
+    insertConfStub.restore()
     done()
   })
 
@@ -64,16 +75,24 @@ describe('createConfController', function() {
   })
 
   it('should create conf and send email', function(done) {
+    const confUUID = 'long_uuid'
+    const confPin = 123456789
+    insertConfStub = insertConfStub.returns(Promise.resolve({
+      id: confUUID,
+      pin: confPin,
+    }))
+
     chai.request(app)
-    .post(urls.createConf)
-    .type('form')
-      .send({
-        email: 'good.email@beta.gouv.fr',
+      .get(urls.createConf)
+      .query({
+        token: 'long_random_token',
       })
       .end(function(err, res) {
-        testUtils.shouldRedirectToLocation(res, urls.confCreated)
+        sinon.assert.calledOnce(getTokenStub)
         sinon.assert.calledOnce(createConfStub)
+        sinon.assert.calledOnce(insertConfStub)
         sinon.assert.calledOnce(sendEmailStub)
+        testUtils.shouldRedirectToLocation(res, urls.showConf.replace(":id", confUUID) + '#' + confPin)
         done()
       })
   })
