987f41ea89639fc836569f7c25752947add42549
Estelle Comment <estelle.comment@gmail.com>
2020-12-10 18:06:15 +0100

    More tests

--- test/showConfControllerTest.js
+++ test/showConfControllerTest.js
@@ -36,12 +36,71 @@ describe('showConfController', function() {
         sinon.assert.calledOnce(getConfStub)
         chai.expect(res.text).to.have.string('10 décembre 2020')
         chai.expect(res.text).to.have.string('01 22 33 44 55')
+        chai.expect(res.text).to.have.string('toute la journée') // day conf
+
         done()
       })
   })
 
-  it('should show PIN if passed in hash', function(done) {
-    done()
+  it('should redirect to landing page if the conf does not exist', function(done) {
+    const confUUID = 'a-fake-conf-id'
+
+    getConfStub = getConfStub.returns(Promise.reject(new Error('conf does not exist dude')))
+
+    chai.request(app)
+      .get(urls.showConf)
+      .redirects(0) // block redirects, we don't want to test them
+      .query({
+        id: confUUID,
+      })
+      .end(function(err, res) {
+        sinon.assert.calledOnce(getConfStub)
+        res.should.redirectTo(urls.landing)
+        done()
+      })
   })
 
+  it('should redirect to landing page if the conf is expired', function(done) {
+    const confUUID = 'an-expired-conf-id'
+
+    getConfStub = getConfStub.returns(Promise.resolve())
+
+    chai.request(app)
+      .get(urls.showConf)
+      .redirects(0) // block redirects, we don't want to test them
+      .query({
+        id: confUUID,
+      })
+      .end(function(err, res) {
+        sinon.assert.calledOnce(getConfStub)
+        res.should.redirectTo(urls.landing)
+        done()
+      })
+  })
+
+  it('should redirect to landing page if the conf is canceled', function(done) {
+    const confUUID = 'a-canceled-conf-id'
+    const conferenceDay = '2020-12-10'
+    const phoneNumber = '0033122334455'
+
+    getConfStub = getConfStub.returns(Promise.resolve({
+      id: confUUID,
+      email: 'martin.rauch@ddr.de',
+      phoneNumber: phoneNumber,
+      expiresAt: new Date(`${conferenceDay} 23:59:59 GMT+3`),
+      canceledAt: new Date(),
+    }))
+
+    chai.request(app)
+      .get(urls.showConf)
+      .redirects(0) // block redirects, we don't want to test them
+      .query({
+        id: confUUID,
+      })
+      .end(function(err, res) {
+        sinon.assert.calledOnce(getConfStub)
+        res.should.redirectTo(urls.landing)
+        done()
+      })
+    })
 })
