90933245ddabc2fa1f21661ca8a6a56743c4884b
Estelle Comment <estelle.comment@gmail.com>
2020-12-11 15:00:18 +0100

    Standardise stub outputs

--- test/createConfControllerTest.js
+++ test/createConfControllerTest.js
@@ -35,6 +35,36 @@ describe('createConfController', function() {
     done()
   })
 
+  const setupStubsForSuccessCase = (conference) => {
+    // Todo : use typescript to return a Token DB object
+    getTokenStub = getTokenStub.returns(Promise.resolve([{
+      token: 'long_random_token',
+      email: conference.email,
+      conferenceDay: conference.conferenceDay,
+      userTimezoneOffset: conference.userTimezoneOffset,
+      createdAt: new Date(),
+      expiresAt: new Date(),
+    }]))
+
+    // Assumes Rooms API
+    createConfStub = createConfStub.returns(Promise.resolve({
+      phoneNumber: conference.phoneNumber,
+      pin: conference.pin,
+      freeAt: conference.freeAt
+    }))
+
+    // Todo : use typescript to return a Conference DB object
+    insertConfStub = insertConfStub.returns(Promise.resolve({
+      id: conference.id,
+      email: conference.email,
+      phoneNumber: conference.phoneNumber,
+      createdAt: new Date(),
+      expiresAt: conference.freeAt,
+    }))
+
+    sendEmailStub = sendEmailStub.returns(Promise.resolve())
+  }
+
   it('should create conf and send email', function(done) {
     const conference = {
       id: 'long_uuid',
@@ -45,22 +75,7 @@ describe('createConfController', function() {
       userTimezoneOffset: '-180',
       freeAt: new Date(),
     }
-    getTokenStub = getTokenStub.returns(Promise.resolve([{
-      email: conference.email,
-      conferenceDay: conference.conferenceDay,
-      userTimezoneOffset: conference.userTimezoneOffset,
-    }]))
-    createConfStub = createConfStub.returns(Promise.resolve(
-      { phoneNumber: conference.phoneNumber, pin: conference.pin, freeAt: conference.freeAt }))
-    // Todo : use typescript to return a Conference DB object
-    insertConfStub = insertConfStub.returns(Promise.resolve({
-      id: conference.id,
-      email: conference.email,
-      phoneNumber: conference.phoneNumber,
-      createdAt: new Date(),
-      expiresAt: conference.freeAt,
-    }))
-    sendEmailStub = sendEmailStub.returns(Promise.resolve())
+    setupStubsForSuccessCase(conference)
 
     chai.request(app)
       .get(urls.createConf)
@@ -89,55 +104,42 @@ describe('createConfController', function() {
       userTimezoneOffset: '-180',
       freeAt: new Date(),
     }
-    getTokenStub = getTokenStub.returns(Promise.resolve([{
-      email: conference.email,
-      conferenceDay: conference.conferenceDay,
-      userTimezoneOffset: conference.userTimezoneOffset,
-    }]))
-    createConfStub = createConfStub.returns(Promise.resolve(
-      { phoneNumber: conference.phoneNumber, pin: conference.pin, freeAt: conference.freeAt }))
-    // Todo : use typescript to return a Conference DB object
-    insertConfStub = insertConfStub.returns(Promise.resolve({
-      id: conference.id,
-      email: conference.email,
-      phoneNumber: conference.phoneNumber,
-      createdAt: new Date(),
-      expiresAt: conference.freeAt,
-    }))
-    sendEmailStub = sendEmailStub.returns(Promise.resolve())
+    setupStubsForSuccessCase(conference)
 
     chai.request(app)
-    .get(urls.createConf)
-    .redirects(0) // block redirects, we don't want to test them
-    .query({
-      token: 'long_random_token',
-    })
-    .end(function(err, res) {
-      sinon.assert.calledOnce(sendEmailStub)
-      const obtainedConference = sendEmailStub.getCall(0).args[0]
+      .get(urls.createConf)
+      .redirects(0) // block redirects, we don't want to test them
+      .query({
+        token: 'long_random_token',
+      })
+      .end(function(err, res) {
+        sinon.assert.calledOnce(sendEmailStub)
+        const obtainedConference = sendEmailStub.getCall(0).args[0]
 
-      chai.assert.equal(obtainedConference.conferenceDay, conference.conferenceDay)
-      chai.assert.equal(obtainedConference.userTimezoneOffset, conference.userTimezoneOffset)
-      chai.assert.equal(obtainedConference.phoneNumber, conference.phoneNumber)
-      chai.assert.equal(obtainedConference.pin, conference.pin)
-      chai.assert.equal(obtainedConference.email, conference.email)
+        chai.assert.equal(obtainedConference.conferenceDay, conference.conferenceDay)
+        chai.assert.equal(obtainedConference.userTimezoneOffset, conference.userTimezoneOffset)
+        chai.assert.equal(obtainedConference.phoneNumber, conference.phoneNumber)
+        chai.assert.equal(obtainedConference.pin, conference.pin)
+        chai.assert.equal(obtainedConference.email, conference.email)
 
-      done()
-    })
+        done()
+      })
   })
 
   it('should redirect when token is bad', function(done) {
-    const confUUID = 'long_uuid'
-    const confPin = 123456789
-    insertConfStub = insertConfStub.returns(Promise.resolve({
-      id: confUUID,
-      pin: confPin,
-    }))
-    createConfStub = createConfStub.returns(Promise.resolve(
-      { phoneNumber: '+330122334455', pin: confPin, freeAt: new Date() }))
-    sendEmailStub = sendEmailStub.returns(Promise.resolve())
+    const conference = {
+      id: 'long_uuid',
+      phoneNumber: '330122334455',
+      pin: 123456789,
+      email: 'good.email@thing.com',
+      conferenceDay: '2020-12-09',
+      userTimezoneOffset: '-180',
+      freeAt: new Date(),
+    }
+    setupStubsForSuccessCase(conference)
     // No token found.
-    getTokenStub = getTokenStub.returns(Promise.resolve([]))
+    getTokenStub.restore()
+    getTokenStub = sinon.stub(db, 'getToken').returns(Promise.resolve([]))
 
     chai.request(app)
       .get(urls.createConf)
@@ -156,20 +158,21 @@ describe('createConfController', function() {
   })
 
   it('should redirect when conf was not created', function(done) {
-    const confUUID = 'long_uuid'
-    const confPin = 123456789
-    insertConfStub = insertConfStub.returns(Promise.resolve({
-      id: confUUID,
-      pin: confPin,
-    }))
-    getTokenStub = getTokenStub.returns(Promise.resolve([{
+    const conference = {
+      id: 'long_uuid',
+      phoneNumber: '330122334455',
+      pin: 123456789,
       email: 'good.email@thing.com',
       conferenceDay: '2020-12-09',
       userTimezoneOffset: '-180',
-    }]))
-    sendEmailStub = sendEmailStub.returns(Promise.resolve())
+      freeAt: new Date(),
+    }
+    setupStubsForSuccessCase(conference)
+
     // Conf creation errors
-    createConfStub = createConfStub.returns(Promise.reject(new Error('Conf not created aaaaah')))
+    createConfStub.restore()
+    createConfStub = sinon.stub(conferences, 'createConf')
+      .returns(Promise.reject(new Error('Conf not created aaaaah')))
 
     chai.request(app)
       .get(urls.createConf)
@@ -188,20 +191,21 @@ describe('createConfController', function() {
   })
 
   it('should redirect when email was not sent', function(done) {
-    const confUUID = 'long_uuid'
-    const confPin = 123456789
-    getTokenStub = getTokenStub.returns(Promise.resolve([{
+    const conference = {
+      id: 'long_uuid',
+      phoneNumber: '330122334455',
+      pin: 123456789,
       email: 'good.email@thing.com',
       conferenceDay: '2020-12-09',
       userTimezoneOffset: '-180',
-    }]))
-    createConfStub = createConfStub.returns(Promise.resolve(
-      { phoneNumber: '+330122334455', pin: confPin, freeAt: new Date() }))
-    insertConfStub = insertConfStub.returns(Promise.resolve({
-      id: confUUID,
-      pin: confPin,
-    }))
-    sendEmailStub = sendEmailStub.returns(Promise.reject(new Error('oh no email not sent')))
+      freeAt: new Date(),
+    }
+    setupStubsForSuccessCase(conference)
+
+    // Email error when sending
+    sendEmailStub.restore()
+    sendEmailStub = sinon.stub(emailer, 'sendConfCreatedEmail')
+      .returns(Promise.reject(new Error('oh no email not sent')))
 
     chai.request(app)
       .get(urls.createConf)
