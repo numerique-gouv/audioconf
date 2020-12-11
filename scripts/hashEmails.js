4208926fd2e5fa3677841ff3dfa15103b1551590
pom421 <mauguet.po@gmail.com>
2020-12-11 18:42:39 +0100

    doc: add commentary on script

--- scripts/hashEmails.js
+++ scripts/hashEmails.js
@@ -1,4 +1,6 @@
+// SCRIPT TO RUN IF WE WANT TO HASH EMAILS FOR OLD ROWS GLOBALLY
+
 // script to run manually the job.
-const computeHashedEmail = require("../jobs/computeHashedEmail")
+const computeHashedEmail = require("../jobs/hashEmails")
 
 computeHashedEmail()
