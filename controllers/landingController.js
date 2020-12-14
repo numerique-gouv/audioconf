4745ac3ad9c52d1f6b196f6b43c29bf3800e2807
pom421 <mauguet.po@gmail.com>
2020-12-14 20:01:52 +0100

    chore: remove number management

--- controllers/landingController.js
+++ controllers/landingController.js
@@ -1,6 +1,6 @@
-const config = require('../config')
-const db = require('../lib/db')
-const format = require('../lib/format')
+const config = require("../config")
+const db = require("../lib/db")
+const format = require("../lib/format")
 
 const areStatsTooOldToDiplay = stats => {
   const STATS_MAX_AGE_MINUTES = 5
@@ -13,22 +13,17 @@ const areStatsTooOldToDiplay = stats => {
 }
 
 module.exports.getLanding = async (req, res) => {
-  const phoneNumbers = await db.getPhoneNumberList()
-  const now = new Date()
-  const numberOfFreePhoneNumbers = phoneNumbers.filter(phoneNumber => phoneNumber.freeAt < now).length
-  const nextFreePhoneNumberAt = phoneNumbers[0] ? phoneNumbers[0].freeAt : new Date()
-
   let statsPoint = {}
   let displayStats = config.FEATURE_DISPLAY_STATS_ON_LANDING
   if (displayStats) {
     try {
       statsPoint = (await db.getLatestStatsPoints(1))[0]
       if (areStatsTooOldToDiplay(statsPoint)) {
-        console.log('Stats too old to display, stats date is', statsPoint.date)
+        console.log("Stats too old to display, stats date is", statsPoint.date)
         displayStats = false
       }
     } catch (err) {
-      console.error(`Impossible de récupérer le statsPoint, donc on ne l'affiche pas.`, err)
+      console.error("Impossible de récupérer le statsPoint, donc on ne l'affiche pas.", err)
       displayStats = false
     }
   }
@@ -39,9 +34,9 @@ module.exports.getLanding = async (req, res) => {
   }
   const dateChoices = []
   let date = new Date()
-  dateChoices.push({ label: 'Aujourd\'hui', value: format.formatStandardDate(date) })
+  dateChoices.push({ label: "Aujourd'hui", value: format.formatStandardDate(date) })
   date = incrementDate(date)
-  dateChoices.push({ label: 'Demain', value: format.formatStandardDate(date) })
+  dateChoices.push({ label: "Demain", value: format.formatStandardDate(date) })
 
   const numOtherDates = (config.RESERVE_NUM_DAYS_AHEAD - 2) > 0 ? (config.RESERVE_NUM_DAYS_AHEAD - 2) : 0
   for (let i = 0 ; i < numOtherDates; i++) {
@@ -52,10 +47,8 @@ module.exports.getLanding = async (req, res) => {
     })
   }
 
-  res.render('landing', {
+  res.render("landing", {
     NUM_PIN_DIGITS: config.NUM_PIN_DIGITS,
-    numberOfFreePhoneNumbers: numberOfFreePhoneNumbers,
-    nextFreePhoneNumberAt: nextFreePhoneNumberAt,
     CONFERENCE_MAX_DURATION_IN_MINUTES: config.CONFERENCE_MAX_DURATION_IN_MINUTES,
     FEATURE_DISPLAY_STATS_ON_LANDING: displayStats,
     onlineParticipantsCount: statsPoint.onlineParticipantsCount,
