2ff8ef18c0254c5f44de1c6b872464fdec485e4c
pom421 <pierre-olivier.mauguet@beta.gouv.fr>
2020-12-23 18:10:49 +0100

    MEP v1.7.0 (#113)

--- test/createConfControllerTest.js
+++ test/createConfControllerTest.js
@@ -1,28 +1,24 @@
-const chai = require('chai')
-const sinon = require('sinon')
+const chai = require("chai")
+const sinon = require("sinon")
 
-const app = require('../index')
-const conferences = require('../lib/conferences')
-const config = require('../config')
-const db = require('../lib/db')
-const emailer = require('../lib/emailer')
-const urls = require('../urls')
+const app = require("../index")
+const conferences = require("../lib/conferences")
+const db = require("../lib/db")
+const emailer = require("../lib/emailer")
+const urls = require("../urls")
 
-describe('createConfController', function() {
+describe("createConfController", function() {
   let createConfStub
   let sendEmailStub
   let getTokenStub
   let insertConfStub
-  let USE_OVH_ROOM_API_BCK
 
   beforeEach(function(done) {
-    createConfStub = sinon.stub(conferences, 'createConf')
-    sendEmailStub = sinon.stub(emailer, 'sendConfCreatedEmail')
-    getTokenStub = sinon.stub(db, 'getToken')
-    insertConfStub = sinon.stub(db, 'insertConferenceWithFreeAt')
+    createConfStub = sinon.stub(conferences, "createConf")
+    sendEmailStub = sinon.stub(emailer, "sendConfCreatedEmail")
+    getTokenStub = sinon.stub(db, "getToken")
+    insertConfStub = sinon.stub(db, "insertConferenceWithFreeAt")
 
-    USE_OVH_ROOM_API_BCK = config.USE_OVH_ROOM_API
-    config.USE_OVH_ROOM_API = true
     done()
   })
 
@@ -31,21 +27,20 @@ describe('createConfController', function() {
     sendEmailStub.restore()
     getTokenStub.restore()
     insertConfStub.restore()
-    config.USE_OVH_ROOM_API = USE_OVH_ROOM_API_BCK
     done()
   })
 
-  it('should create conf and send email', function(done) {
-    const confUUID = 'long_uuid'
+  it("should create conf and send email", function(done) {
+    const confUUID = "long_uuid"
     const confPin = 123456789
-    const email = 'good.email@thing.com'
+    const email = "good.email@thing.com"
     getTokenStub = getTokenStub.returns(Promise.resolve([{
       email,
-      conferenceDay: '2020-12-09',
-      userTimezoneOffset: '-180',
+      conferenceDay: "2020-12-09",
+      userTimezoneOffset: "-180",
     }]))
     createConfStub = createConfStub.returns(Promise.resolve(
-      { phoneNumber: '+330122334455', pin: confPin, freeAt: new Date() }))
+      { phoneNumber: "+330122334455", pin: confPin, freeAt: new Date() }))
     insertConfStub = insertConfStub.returns(Promise.resolve({
       id: confUUID,
       pin: confPin,
@@ -56,7 +51,7 @@ describe('createConfController', function() {
       .get(urls.createConf)
       .redirects(0) // block redirects, we don't want to test them
       .query({
-        token: 'long_random_token',
+        token: "long_random_token",
       })
       .end(function(err, res) {
         sinon.assert.calledOnce(getTokenStub)
@@ -64,20 +59,20 @@ describe('createConfController', function() {
         sinon.assert.calledOnce(insertConfStub)
         chai.assert(insertConfStub.getCall(0).calledWith(email))
         sinon.assert.calledOnce(sendEmailStub)
-        res.should.redirectTo(urls.showConf.replace(":id", confUUID) + '#' + confPin)
+        res.should.redirectTo(urls.showConf.replace(":id", confUUID) + "#" + confPin)
         done()
       })
   })
 
-  it('should redirect when token is bad', function(done) {
-    const confUUID = 'long_uuid'
+  it("should redirect when token is bad", function(done) {
+    const confUUID = "long_uuid"
     const confPin = 123456789
     insertConfStub = insertConfStub.returns(Promise.resolve({
       id: confUUID,
       pin: confPin,
     }))
     createConfStub = createConfStub.returns(Promise.resolve(
-      { phoneNumber: '+330122334455', pin: confPin, freeAt: new Date() }))
+      { phoneNumber: "+330122334455", pin: confPin, freeAt: new Date() }))
     sendEmailStub = sendEmailStub.returns(Promise.resolve())
     // No token found.
     getTokenStub = getTokenStub.returns(Promise.resolve([]))
@@ -86,7 +81,7 @@ describe('createConfController', function() {
       .get(urls.createConf)
       .redirects(0) // block redirects, we don't want to test them
       .query({
-        token: 'long_random_token',
+        token: "long_random_token",
       })
       .end(function(err, res) {
         sinon.assert.calledOnce(getTokenStub)
@@ -98,27 +93,27 @@ describe('createConfController', function() {
       })
   })
 
-  it('should redirect when conf was not created', function(done) {
-    const confUUID = 'long_uuid'
+  it("should redirect when conf was not created", function(done) {
+    const confUUID = "long_uuid"
     const confPin = 123456789
     insertConfStub = insertConfStub.returns(Promise.resolve({
       id: confUUID,
       pin: confPin,
     }))
     getTokenStub = getTokenStub.returns(Promise.resolve([{
-      email: 'good.email@thing.com',
-      conferenceDay: '2020-12-09',
-      userTimezoneOffset: '-180',
+      email: "good.email@thing.com",
+      conferenceDay: "2020-12-09",
+      userTimezoneOffset: "-180",
     }]))
     sendEmailStub = sendEmailStub.returns(Promise.resolve())
     // Conf creation errors
-    createConfStub = createConfStub.returns(Promise.reject(new Error('Conf not created aaaaah')))
+    createConfStub = createConfStub.returns(Promise.reject(new Error("Conf not created aaaaah")))
 
     chai.request(app)
       .get(urls.createConf)
       .redirects(0) // block redirects, we don't want to test them
       .query({
-        token: 'long_random_token',
+        token: "long_random_token",
       })
       .end(function(err, res) {
         sinon.assert.calledOnce(getTokenStub)
@@ -130,27 +125,27 @@ describe('createConfController', function() {
       })
   })
 
-  it('should redirect when email was not sent', function(done) {
-    const confUUID = 'long_uuid'
+  it("should redirect when email was not sent", function(done) {
+    const confUUID = "long_uuid"
     const confPin = 123456789
     getTokenStub = getTokenStub.returns(Promise.resolve([{
-      email: 'good.email@thing.com',
-      conferenceDay: '2020-12-09',
-      userTimezoneOffset: '-180',
+      email: "good.email@thing.com",
+      conferenceDay: "2020-12-09",
+      userTimezoneOffset: "-180",
     }]))
     createConfStub = createConfStub.returns(Promise.resolve(
-      { phoneNumber: '+330122334455', pin: confPin, freeAt: new Date() }))
+      { phoneNumber: "+330122334455", pin: confPin, freeAt: new Date() }))
     insertConfStub = insertConfStub.returns(Promise.resolve({
       id: confUUID,
       pin: confPin,
     }))
-    sendEmailStub = sendEmailStub.returns(Promise.reject(new Error('oh no email not sent')))
+    sendEmailStub = sendEmailStub.returns(Promise.reject(new Error("oh no email not sent")))
 
     chai.request(app)
       .get(urls.createConf)
       .redirects(0) // block redirects, we don't want to test them
       .query({
-        token: 'long_random_token',
+        token: "long_random_token",
       })
       .end(function(err, res) {
         sinon.assert.calledOnce(getTokenStub)
---
+++ test/utils.js
@@ -0,0 +1,98 @@
+const { parse } = require("pg-connection-string")
+
+const config = require("../config")
+const { Client } = require("pg")
+const knex = require("../knex-client")
+
+const getConnectionStringForDB = ({ user, password, host, port, database }) => `postgres://${encodeURIComponent(user)}:${encodeURIComponent(password)}@${encodeURIComponent(host)}:${encodeURIComponent(port)}/${database}`
+
+function getClientDefaultDB(url) {
+    const { user, password, host, port, database } = parse(url)
+
+    if (!database) return new Error("DATABASE_URL environment variable not set")
+
+    // Postgres needs to have a connection to an existing database in order to perform any request.
+    // Since our test database doesn't exist yet, we need to connect to the default database to create it.
+    const connectionString = getConnectionStringForDB({user, password, host, port, database: "postgres"})
+
+    return {
+        database,
+        client: new Client({ connectionString })
+    }
+}
+
+function setupTestDatabase() {
+    const { database: testDbName, client } = getClientDefaultDB(config.DATABASE_URL)
+
+    console.log(`Test database ${testDbName} is going to be created ...`)
+
+    return client.connect()
+        .then(() => client.query(`DROP DATABASE IF EXISTS ${testDbName}`, []))
+        .then(() => client.query(`CREATE DATABASE ${testDbName}`, []))
+        .then(() => console.log(`Test database ${testDbName} created successfully 🚀`))
+        .then(() => client.end())
+        .then(() => knex.migrate.latest())
+        .then(() => console.log(`Knex migration is successful 🚀`))
+        .catch((error) => console.error("Error in setupTestDatabase", error))
+}
+
+function cleanUpTestDatabase() {
+    const { database: testDbName, client } = getClientDefaultDB(config.DATABASE_URL)
+
+    console.log(`Test database ${testDbName} is going to be deleted ...`)
+
+    return knex.destroy()
+        .then(() => client.connect())
+        .then(() => client.query(`DROP DATABASE IF EXISTS ${testDbName}`, []))
+        .then(() => client.end())
+        .then(() => console.log(`Test database ${testDbName} cleaned up successfully 🚀`))
+        .catch((error) => console.error("Erreur dans cleanupTestDatabase", error))
+}
+
+async function recreatePublicSchema() {
+    await knex.raw("drop schema public cascade")
+    await knex.raw("create schema public")
+}
+
+async function reinitializeDB() {
+    try {
+        await recreatePublicSchema()
+        await knex.migrate.latest({})
+    } catch (error) {
+        console.error("Error in reinitializeDB", error)
+    }
+
+    console.log("Data are reinitialized")
+}
+
+
+/**
+ * Insert a bunch of conferences.
+ *
+ * @param {*} conferences  array of conferences
+ */
+async function insertConferences(conferences = []) {
+    for (const conference of conferences) {
+        await knex("conferences").insert(conference)
+    }
+}
+
+/**
+ * Insert a bunch of users.
+ *
+ * @param {*} conferences  array of users
+ */
+async function insertUsers(users = []) {
+    for (const user of users) {
+        await knex("users").insert(user)
+    }
+}
+
+module.exports = {
+    getConnectionStringForDB,
+    setupTestDatabase,
+    cleanUpTestDatabase,
+    insertConferences,
+    insertUsers,
+    reinitializeDB
+}
