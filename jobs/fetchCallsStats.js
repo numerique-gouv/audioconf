8b10f2061b416cb1733ffc03e9c3127619e1ebb9
Estelle Comment <estelle.comment@gmail.com>
2021-06-21 17:09:38 +0200

    Refactor

--- jobs/fetchCallsStats.js
+++ jobs/fetchCallsStats.js
@@ -1,18 +1,46 @@
 const conferences = require("../lib/conferences")
 const db = require("../lib/db")
 
+const fetchCallIds = async (phoneNumber) => {
+  const latestRecordedCall = await db.getLastSuccessfulCallStatsJob(phoneNumber)
+  const intervalStartDate = latestRecordedCall ? latestRecordedCall.dateBegin.toISOString() : undefined
+  console.log("Fetching calls for phone number", phoneNumber, "from", intervalStartDate, "to now")
+  const callIds = await conferences.getCallsForPhoneNumber(phoneNumber, intervalStartDate)
+  console.log("Got", callIds.length, "calls")
+  return callIds
+}
+
+const insertCallHistory = async (history, phoneNumber, JOB_DRY_RUN, summary) => {
+  if (JOB_DRY_RUN) {
+    console.log(`${history.id} not inserted, dry run`)
+    summary.insertedRows++
+  } else {
+    const id = await db.insertCallHistory(phoneNumber, history)
+    if (id) {
+      console.log(`${id} inserted`)
+      summary.insertedRows++
+    } else {
+      console.log(`${phoneNumber}_${history.id} already inserted`)
+      summary.alreadyInsertedRows++
+    }
+  }
+}
+
+const recordSuccessfulJob = async (phoneNumber) => {
+  const lastCall = await db.getLatestCallHistory(phoneNumber)
+  await db.insertLastSuccessfulCallStatsJob(phoneNumber, lastCall)
+}
+
 module.exports = async () => {
   console.debug("Start of fetchCallsStats job")
 
   const JOB_DRY_RUN = process.env.JOB_DRY_RUN === "true"
   console.log("Dry run :", JOB_DRY_RUN)
-
   const JOB_CALLS_STATS_SMALL_RUN = process.env.JOB_CALLS_STATS_SMALL_RUN === "true"
   console.log("Small run :", JOB_CALLS_STATS_SMALL_RUN)
 
   const phoneNumbers = await conferences.getAllPhoneNumbers()
   console.log("Got", phoneNumbers.length, "phone numbers.")
-
   if (!phoneNumbers.length) {
     console.log("No numbers found. Check your configuration.")
     process.exit(1)
@@ -30,34 +58,15 @@ module.exports = async () => {
   const numPhoneNumbersToRun = JOB_CALLS_STATS_SMALL_RUN ? 2 : phoneNumbers.length
 
   for (const phoneNumber of sortedPhoneNumbers.slice(0, numPhoneNumbersToRun)) {
-    const latestRecordedCall = await db.getLastSuccessfulCallStatsJob(phoneNumber)
-    const intervalStartDate = latestRecordedCall ? latestRecordedCall.dateBegin.toISOString() : undefined
-
-    console.log("Fetching calls for phone number", phoneNumber, "from", intervalStartDate, "to now")
-    const callIds = await conferences.getCallsForPhoneNumber(phoneNumber, intervalStartDate)
-    console.log("Got", callIds.length, "calls")
+    const callIds = await fetchCallIds(phoneNumber)
 
     for (const callId of callIds) {
       const history = await conferences.getHistoryForCall(phoneNumber, callId)
-
-      if (JOB_DRY_RUN) {
-        console.log(`${history.id} not inserted, dry run`)
-        summary.insertedRows++
-      } else {
-        const id = await db.insertCallHistory(phoneNumber, history)
-        if (id) {
-          console.log(`${id} inserted`)
-          summary.insertedRows++
-        } else {
-          console.log(`${phoneNumber}_${history.id} already inserted`)
-          summary.alreadyInsertedRows++
-        }
-      }
+      await insertCallHistory(history, phoneNumber, JOB_DRY_RUN, summary)
     }
 
     // Record that this batch of inserts was successful
-    const lastCall = await db.getLatestCallHistory(phoneNumber)
-    await db.insertLastSuccessfulCallStatsJob(phoneNumber, lastCall)
+    await recordSuccessfulJob(phoneNumber)
   }
 
   console.dir({ summary })
