79825bc2761d00db0786c156da6a303b0998e76d
lucharrier <lucharrier@gmail.com>
2021-08-10 17:28:31 +0200

    Added files

---
+++ controllers/dashboardController.js
@@ -0,0 +1,31 @@
+
+const jwt = require("jsonwebtoken")
+
+const config = require("../config")
+const conferences = require("../lib/conferences")
+
+module.exports.get = async (req, res) => {
+    const token = req.params.token
+
+    if (!token) {
+        res.redirect("/")
+    }
+
+    try {
+        const pin = jwt.verify(token, config.SECRET)
+        const room = conferences.getRoom(pin)
+        console.log('Get room', room)
+        if (!room) {
+            throw new Error(`Cette roon n'existe plus ou pas`)
+            
+        } else {
+            return res.render("dashboard", {
+                room
+            })
+        }
+    } catch (err) {
+        console.log(`Impossible de recuperer la room : ${err}`)
+        req.flash(`Ce lien n'est plus valide : ${err}`)
+        res.redirect("/")
+    }
+}
