0f4758527dba13e26d6c9e41267a73d8515c4a74
lucharrier <lucharrier@gmail.com>
2021-12-14 19:24:23 +0100

    Fetch participants every 5 seconds

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -7,7 +7,7 @@ window.onload = function() {
         var currentDate = new Date()
         var id = document.getElementById("last-update")
         id.innerText = Math.floor((currentDate.getTime() - initDate.getTime())/1000)
-    }, 1000)
+    }, 5000)
 }
 
 function postRequest(url, token, callback) {
